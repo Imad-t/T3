a428d2118179c8cbd58ab397ced9fb1685528464
Imad-t <imadeddineterraf@gmail.com>
2024-08-01 02:41:52 +0100

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { postcss } from "tailwindcss";
+import { db } from "~/server/db";
+
 const imgUrls = ["https://utfs.io/f/647fd81f-c235-4cba-af29-53280d0e6514-ve28ll.png",
   "https://utfs.io/f/d7f751a9-d0ee-41f3-86d4-2848ecb4a302-jg9rdf.png",
   "https://utfs.io/f/bffbe71e-8021-4e85-ac75-8b8c14a9f107-wkf9ly.png",
@@ -8,11 +11,15 @@ const mockImages = imgUrls.map((url, index) => ({
   url
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
    <main >
-    <div className="flex flex-wrap gap-2">
-    {mockImages.map((image) => (
+    <div className="flex flex-wrap gap-1 p-1">
+    {posts.map((post) => (
+      <div key={post.id} className="flex items-center">{post.name}</div>
+      ))}  
+    {mockImages.map((image, index ) => (
         <img key={image.id} src={image.url} className="h-48" alt="image"/>
     
     ))}
